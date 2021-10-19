17e6ab2006254562b8b269ab55f5a77a43adef45
user-nameAleksey <alesej88@ya.ru>
2021-10-19 08:33:59 +0600

    app is done

---
+++ src/main/java/Client/ClientApp.java
@@ -0,0 +1,14 @@
+package Client;
+
+import java.io.IOException;
+
+public class ClientApp {
+            public static void main(String[] args) {
+            try {
+                new Client().run(System.in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
---
+++ src/main/java/Client/ClientConnectionException.java
@@ -0,0 +1,12 @@
+package Client;
+
+public class ClientConnectionException extends RuntimeException {
+    public ClientConnectionException(String message) {
+        super(message);
+    }
+
+    public ClientConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
